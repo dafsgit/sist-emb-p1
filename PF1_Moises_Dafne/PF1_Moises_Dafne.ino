6b0bc43616f524bef96dfbbfc87aa158486ab725
dafsgit <A01732610@tec.mx>
2023-03-24 10:35:54 -0600

    keypad and format

--- PF1_Moises_Dafne/PF1_Moises_Dafne.ino
+++ PF1_Moises_Dafne/PF1_Moises_Dafne.ino
@@ -3,137 +3,129 @@
 
 #include <Arduino_FreeRTOS.h>
 #include "queue.h"
+#include <Keypad.h>                     // https://github.com/Chris--A/Keypad
 
 #define S1  2
 #define S2  3
 #define S3  4
 
+const byte ROWS = 4;                    // Rows on the keypad
+const byte COLS = 4;                    // Columns on the keypad
+char keys[ROWS][COLS] = {               // Define the keypad matrix
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte rowPins[ROWS] = { 12, 11, 10, 9 }; // Row pins of the keypad
+byte colPins[COLS] = { 8, 7, 6, 5 };    // Column pins of the keypad
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 QueueHandle_t serialGKQueue;
-struct sen{
+struct sen {
    int8_t id;
    int8_t value;
 };
 
-void setup()
-{
-  //open serial port
+void setup() {
   Serial.begin(9600);
   
-  //creation of tasks
+  // Creation of tasks
   xTaskCreate(vS1Task,        "SENSOR 1 TASK",          100, NULL, 2, NULL);
   xTaskCreate(vS2Task,        "SENSOR 2 TASK",          100, NULL, 2, NULL);
   xTaskCreate(vS3Task,        "SENSOR 3 TASK",          100, NULL, 2, NULL);
   xTaskCreate(vGKSerialTask,  "SERIAL GATEKEEPER TASK", 100, NULL, 1, NULL);
   
-  //creation of the queue
+  // Creation of the queue
   serialGKQueue = xQueueCreate(5, sizeof(sen));
     
-  //check if queue has been created
-  if(serialGKQueue == NULL){
+  // Check if queue has been created
+  if (serialGKQueue == NULL){
     Serial.println("Queue not created");
     while(1);
   }
 }
 
-void vS1Task(void * pvParameters)
-{  
+void vS1Task(void * pvParameters) {  
   sen infoToSend;
   infoToSend.id = 1;
   BaseType_t qStatus;
 
-  while(1)
-  {    
+  while(1) {    
     infoToSend.value = digitalRead(S1);
     
     qStatus = xQueueSend(serialGKQueue, &infoToSend, portMAX_DELAY);
 
-    if(qStatus != pdPASS)
-    {
+    if(qStatus != pdPASS) {
       Serial.println("S1 data NOT sent to queue"); 
-    }
-    else
-    {
+    } else {
       Serial.print("S1 sent to queue: ");
       Serial.println(infoToSend.value);
     }
     
-    //necessary for the operation of the serial monitor
+    // Necessary for the operation of the serial monitor
     vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
-void vS2Task(void * pvParameters)
-{  
+void vS2Task(void * pvParameters) {  
   sen infoToSend;
   infoToSend.id = 2;
   BaseType_t qStatus;
 
-  while(1)
-  {    
+  while(1) {    
     infoToSend.value = digitalRead(S2);
     
     qStatus = xQueueSend(serialGKQueue, &infoToSend, portMAX_DELAY);
 
-    if(qStatus != pdPASS)
-    {
+    if(qStatus != pdPASS) {
       Serial.println("S2 data NOT sent to queue"); 
-    }
-    else
-    {
+    } else {
       Serial.print("S2 sent to queue: ");
       Serial.println(infoToSend.value);
     }
     
-    //necessary for the operation of the serial monitor
+    // Necessary for the operation of the serial monitor
     vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
-void vS3Task(void * pvParameters)
-{  
+void vS3Task(void * pvParameters) { 
   sen infoToSend;
   infoToSend.id = 3;
   BaseType_t qStatus;
 
-  while(1)
-  {    
+  while(1) {    
     infoToSend.value = digitalRead(S3);
     
     qStatus = xQueueSend(serialGKQueue, &infoToSend, portMAX_DELAY);
 
-    if(qStatus != pdPASS)
-    {
+    if(qStatus != pdPASS) {
       Serial.println("S3 data NOT sent to queue"); 
-    }
-    else
-    {
+    } else {
       Serial.print("S3 sent to queue: ");
       Serial.println(infoToSend.value);
     }
     
-    //necessary for the operation of the serial monitor
+    // Necessary for the operation of the serial monitor
     vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
 
-void vGKSerialTask(void * pvParameters)
-{
+void vGKSerialTask(void * pvParameters) {
   sen senInfoReceived;
   
   BaseType_t qStatus;
   //const TickType_t xTicksToWait = pdMS_TO_TICKS(100);
 
-  while(1)
-  {
-    //entering critical section
+  while(1) {
+    // Entering critical section
     taskENTER_CRITICAL();
 
-    //read temperature value (always the first one)
     qStatus = xQueueReceive(serialGKQueue, &senInfoReceived, portMAX_DELAY);
 
-    if(qStatus == pdPASS)
-    {
+    if(qStatus == pdPASS) {
       switch (senInfoReceived.id) {
         case 1:
           Serial.print("S1: ");
@@ -151,19 +143,23 @@ void vGKSerialTask(void * pvParameters)
           Serial.print("Invalid sensor ID");
           break;
       }
-    }
-    else
-    {
+    } else {
       Serial.println("Sensor data NOT received from queue"); 
     }
 
     Serial.println("");
 
-    //exit critical section
+    // Exit critical section
     taskEXIT_CRITICAL();
     
     vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
-void loop() {}
+void loop() {
+  char key = keypad.getKey();
+
+  if (key != NO_KEY) { // When a key is pressed
+    Serial.println(key);
+  }  
+}
